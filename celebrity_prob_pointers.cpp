6c297a7bf353176cb6f43a8b882196e1d3664e88
Akshata Bhat <akshataspb@gmail.com>
2018-11-05 14:01:42 -0800

    Added comments to the celebrity problem code

--- celebrity_prob_pointers.cpp
+++ celebrity_prob_pointers.cpp
@@ -4,6 +4,8 @@ In a party of N people, only one person is known to everyone. Such a person may 
 */
 
 
+// Time complexity using 2 pointers is O(N) unlike other methods(stacks, graphs) having O(N^2)
+
 #include <iostream>
 using namespace std;
 #define N 4
@@ -22,12 +24,16 @@ int check_celebrity(int n)
 	while(a<b)
 	{
 		if(knows(a,b))
-			a++;
+			a++;// If A knows B then, A is not the celebrity, hence move to the next person
+		
 		else
-			b--;
+			b--; // If A does not know B, then A might be the celebrity, now you are checking if he does not know anyone else  
 	}
 
 	//check if the person is a celebrity
+	//(First condition)making sure we are not checking the celebrity with himself
+	//(Second condition)making sure the person does not know anyone, (third condition)but everyone knows him
+
 	for(int i=0; i<n; i++)
 	{
 		if((a!=i) && (knows(a,i)==0 || knows(i,a)!=0))
