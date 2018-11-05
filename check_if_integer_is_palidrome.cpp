5d5a62bca37f633f45f2929f3072d0cec063c974
Akshata Bhat <akshataspb@gmail.com>
2018-11-05 14:15:03 -0800

    Completed the implementation using Stacks only

--- check_if_integer_is_palidrome.cpp
+++ check_if_integer_is_palidrome.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <math.h>
 using namespace std;
 
 
@@ -46,17 +47,47 @@ public:
         cout << endl;
     }
     
-/*
+
     bool isPalindrome(int x) {
-            
+	if(x < 0)
+		return false;
+	else if(x%10 ==0)
+		return false;
+	else{
+		int n=x, remainder, reversed_x=0, val;
+		int count = 0;
+	
+		while(n>0){
+			cout << "n = " << n << "remainder = " << remainder << endl;
+			remainder = n%10;
+			this->pushValue(remainder);
+			cout << "hello";
+			n=n/10;
+			count++;
+		}
+		this->display();
+		int ten_power = 0;	
+		while(count > 0){
+			val = this->popValue();
+			cout << "val = " << val;
+			reversed_x += pow(10,ten_power)*val;
+			cout << "building the number: " << reversed_x << endl;
+			--count;
+			++ten_power;
+		}
+
+		if(reversed_x == x)
+			return true;
+		else 	
+			return false;
+	}     
     }
-*/
 };
 
-int main(){
+int main (int argc, char *argv[]){
 
 	Solution ans1;
-	int val;
+	/*
 	ans1.pushValue(10);
 	ans1.pushValue(20);
 	ans1.pushValue(30);
@@ -71,5 +102,19 @@ int main(){
 
 	cout << "Values on the stack" ;
 	ans1.display();
+	*/
+
+	int a = -2037;
+	int b = 8118;
+	int c = 3403;
+
+	bool result;
+	int number = atoi(argv[1]); 
+	cout << number;
+	result = ans1.isPalindrome(number);
+	if(result)
+		cout << number << " is a Palindrome" << endl;
+	else 
+		cout << number << " is not a Palindrome" << endl;	
 	return 0;
 }
